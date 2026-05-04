5a461225e8de6497a679f0331c90e8b24d660324
Mannyfa <kelvmanuel67@gmail.com>
2026-05-04 13:03:17 +0100

    email services

--- koza-hair-backend/server.js
+++ koza-hair-backend/server.js
@@ -128,7 +128,7 @@ const transporter = nodemailer.createTransport({
     service: process.env.EMAIL_SERVICE || 'gmail',
     host: process.env.EMAIL_HOST,
     port: process.env.EMAIL_PORT,
-    secure: false, 
+    secure: true, 
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
